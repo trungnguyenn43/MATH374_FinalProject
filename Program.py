20a0c03475c2fdebe2faef24cecbd22343872e4f
trungnguyenn43 <trungnguyenn2003@gmail.com>
2024-04-27 19:55:08 -0500

    Add image processing functions and RoadDetect module

--- Program.py
+++ Program.py
@@ -2,55 +2,63 @@
 from skimage import color, io
 import numpy as np
 import matplotlib.pyplot as plt
-#from matplotlib.image import imsave
+from matplotlib.image import imsave
 from scipy.sparse import  spdiags
 from scipy.sparse import eye as speye
 from scipy.sparse import kron as spkron
+
+from RoadDetect import *
 # from scipy.sparse.linalg import spsolve 
-
-##________
-
-
-# ToUint8: Converts a floating point image to a uint8 image -> 0 to 255
-def ToUint8(targetImg):
-   maxVal=np.max(targetImg)
-   minVal=np.min(targetImg)
-   rng=maxVal-minVal
-   # rescales to 0 to 255,  but still a floating point # between 0 and 255
-   newImg= 255*(targetImg-minVal)/rng
-   # Converts the float to uint8
-   newImg=np.uint8(newImg)
-   return newImg
-
-def GreenDropper(img):
-      for pixel in range(img.shape[1]):
-         img[:,:,[1]]=0
-      return img
 
 def DisplayImage(img, num):
    plt.figure(num)
    plt.imshow(img)
    plt.axis('off')
    plt.show()
-   num=num+1
+   return num+1
 
 def SaveImg(img, name):
-   ## Convert to uint8 and save
-   img=ToUint8(img)
-   io.imsave(name, img)
+   newImg=np.uint8(img)
+   io.imsave(name, newImg)
 
+
+def ImageInput(path):
+   img = io.imread(path)
+   if (np.ndim(img)==3): 
+      if (img.shape[2]==4) :
+         img=img[:,:,[0,1,2]]  # 4 channel image to 3 channel
+      else:
+         imgGray=img
+         return imgGray
+   return img
+
+
+# =================================
 # Main script
 # get the image from file
-img1 = io.imread('sample.png')    # read in image  (it is RGB)
-if (np.ndim(img1)==3): 
-   if (img1.shape[2]==4) :
-     img1=img1[:,:,[0,1,2]]  # 4 channel image to 3 channel
-   # img1=GreenDropper(img1)
-else:
-   imgGray=img1
-
-
 figureOrder = 1
-DisplayImage(img1, figureOrder)
-SaveImg(img1, 'Result\Airport.png')
+
+img1 = ImageInput(path='sample3.png')  # read in image  (it is RGB)
+
+# img1 = meanFilter(img1, 3)
+
+# output = RoadDetect(img1, 0.85)
+
+figureOrder = DisplayImage(img1, figureOrder)
+
+R, G, B = ChannelSplit(img1)
+
+#%%
+RG = R + G
+RB = R + B
+GB = G + B
+
+RRG = RG + R
+
+figureOrder = DisplayImage(RRG, figureOrder)
+
+# SaveImg(output, 'Result\Airport.png')
+
+
+
 
---
+++ RoadDetect.py
@@ -0,0 +1,90 @@
+from skimage import color, io
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+
+def RoadDetect(originalImg, level = 0.5):
+    img1R, img1G, img1B, img1Gray = ChannelSplit(originalImg)
+
+    finalOutput = originalImg * 0
+    confidenceLevel = 0
+
+    while(confidenceLevel > level):
+        img1R = img1R - CalMean(img1R)
+        img1G = img1G - CalMean(img1G)
+        img1B = img1B - CalMean(img1B)
+        img1Gray = img1Gray - CalMean(img1Gray)
+        confidenceLevel = confidenceLevel + 0.01
+
+        road = 0
+
+        
+    return finalOutput
+
+
+def ChannelSplit(img):
+
+    imgR= 1 * img
+    imgG= 1 * img
+    imgB= 1 * img
+
+    imgR[:,:,1] = 0
+    imgR[:,:,2] = 0
+
+    imgG[:,:,0] = 0
+    imgG[:,:,2] = 0
+
+    imgB[:,:,0] = 0
+    imgB[:,:,1] = 0
+
+    
+    return imgR, imgG, imgB
+
+def CalMean(img):
+    mean = np.mean(img)
+    return mean
+
+def ToUint8(target):
+    maxVal=np.max(target)
+    minVal=np.min(target)
+    rng=maxVal-minVal   
+    newImg= 255*(target-minVal)/rng    # rescales to 0 to 255,  but still a floating point # between 0 and 255
+    newImg=np.uint8(newImg);     # Converts the float to uint8 
+    return newImg
+    
+def RoadTest(imgR, imgB, imgG):
+    
+    print()
+    
+def meanFilter(myImg,boxRadius=1):
+    imgDim=myImg.shape   # get image dimensions 
+    ny=imgDim[0]
+    nx=imgDim[1]
+    nz=imgDim[2]
+    newImg=np.zeros([ny,nx,nz])  # initialize filtered image 
+    
+    for z in range(nz):
+        for y in range (ny): #current Y coordinate
+            for x in range (nx): #current X coordinate
+                element_count = 0
+                total = 0
+                
+                for y_surround in range(y - boxRadius, y + boxRadius +1):
+                    if y_surround < 0 or y_surround > ny -1:
+                        continue
+                    
+                    for x_surround in range(x - boxRadius, x + boxRadius +1):
+                        if x_surround < 0 or x_surround > nx -1:
+                            continue
+                        else:
+                            element_count += 1
+                            total += myImg[y_surround][x_surround][z]
+                
+                newImg[y][x][z]= total/element_count
+        
+    newImg = ToUint8(newImg)
+    
+    return newImg  # wrong answer!
+    
+    
